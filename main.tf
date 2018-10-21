d61a7b19ecf5fb34123704551107638c6e74d52d
darren-rose <darrenwrose@gmail.com>
2018-10-21 19:42:24 +0200

    formatting

--- main.tf
+++ main.tf
@@ -1,41 +1,45 @@
 provider "kubernetes" {
-	config_context_cluster = "docker-for-desktop-cluster"
-	config_context_auth_info = "docker-for-desktop"
-	username = "docker-for-desktop"
+  config_context_cluster = "docker-for-desktop-cluster"
+  config_context_auth_info = "docker-for-desktop"
+  username = "docker-for-desktop"
 }
 
 resource "kubernetes_pod" "echo" {
   metadata {
-    name = "echo-example"
+    name = "echo-pod"
     labels {
       App = "echo"
-  } }
+    }
+  }
   spec {
     container {
       image = "hashicorp/http-echo:0.2.1"
       name  = "example2"
-      args = ["-listen=:80", "-text='Hello World'"]
+      args = ["-listen=:80", "-text='Hello World Terraform'"]
       port {
         container_port = 80
-} } } }
+      }
+    }
+  }
+}
 
 resource "kubernetes_service" "echo" {
   metadata {
-    name = "echo-example"
+    name = "echo-service"
   }
   spec {
     selector {
       App = "${kubernetes_pod.echo.metadata.0.labels.App}"
     }
     port {
-      port        = 80
+      port        = 10080
       target_port = 80
     }
     type = "LoadBalancer"
 } }
 
 output "lb_ip" {
-  value = "${kubernetes_service.echo.load_balancer_ingress.0.ip}"
+  value = "${kubernetes_service.echo.load_balancer_ingress}"
 }
 
 
